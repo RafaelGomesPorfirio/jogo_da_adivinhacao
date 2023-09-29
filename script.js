7624cab707203419573c4ad9d23e581044ea643e
Rafael <121689279+RafaelGomesPorfirio@users.noreply.github.com>
2023-09-29 00:18:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const screen3 = document.querySelector(".screen3")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+})
+
+// funções
+function handleTryClick(event) {
+    event.preventDefault() // nao faca o padrao
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == randomNumber) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
+    }else if (Number(inputNumber.value) < 0 ) {
+        alert("Tente novamente, o numero informado é menor que zero")
+    }else if (Number(inputNumber.value) > 10) {
+        alert("Tente novamente, o numero informado é maior que dez")
+    } else if (Number(inputNumber.value) == "") {
+        alert("Digite um numero diferente de nulo")
+    }
+        
+    
+
+    inputNumber.value = ""
+    xAttempts++
+}
+
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
